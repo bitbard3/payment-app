aa4774d2c1e3b38c7f67cde3dac9403f19466d30
bitbard3 <ansharora3839@gmail.com>
2024-02-04 09:28:07 +0530

    Add signup and login routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,3 +1,13 @@
 import express from 'express'
+import { userExist } from '../middlewares/userExist.middleware.js'
+import { signup, login } from '../controllers/user.controllers.js'
 
-export const userRouter = express.Router()
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({ msg: "hi" })
+})
+router.post('/signup', userExist, signup)
+router.post('/login', userExist, login)
+
+export default router
