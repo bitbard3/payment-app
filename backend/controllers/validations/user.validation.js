b67f999296d9bc5b0080408c5162341caad44599
bitbard3 <ansharora3839@gmail.com>
2024-02-04 09:25:28 +0530

    Fix bug

--- backend/controllers/validations/user.validation.js
+++ backend/controllers/validations/user.validation.js
@@ -1,8 +1,8 @@
 import z from 'zod'
 
 export const userSchema = z.object({
-    username: z.string().required().min(3).max(30).trim().toLowerCase(),
-    password: z.string().required().min(4),
-    firstName: z.string().required().max(50).trim(),
-    lastName: z.string().required().max(50).trim()
+    username: z.string().min(3).max(30).trim().toLowerCase(),
+    password: z.string().min(4),
+    firstName: z.string().max(50).trim(),
+    lastName: z.string().max(50).trim()
 })
