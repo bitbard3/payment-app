0d807e01c5771d6988e494b1420695c70950faad
bitbard3 <ansharora3839@gmail.com>
2024-02-04 06:56:34 +0530

    Add cors and json parser middllwares

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { router } from "./routes/routes"
+import cors from "cors";
 
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
 app.use('/api/v1', router)
 
 app.listen(port, () => {
