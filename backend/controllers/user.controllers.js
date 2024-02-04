5228ec8bee4a344734f5d81e86933ffc52cb8bc8
bitbard3 <ansharora3839@gmail.com>
2024-02-04 08:34:44 +0530

    Add login route

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,8 +1,10 @@
 import { User } from "../db/db";
-import { userSignupSchema } from "./validations/user.validation.js";
+import { userSchema } from "./validations/user.validation.js";
+import jwt from 'jsonwebtoken'
+require('dotenv').config()
 
 const signup = (req, res) => {
-    const validInput = userSignupSchema.safeParse(req.body)
+    const validInput = userSchema.safeParse(req.body)
     const userExist = req.user
     if (!validInput.success) {
         res.status(411).json({ msg: "Incorrect Inputs" })
@@ -22,4 +24,18 @@ const signup = (req, res) => {
         res.status(409).json({ msg: "User already exist" })
         return
     }
+}
+const login = (req, res) => {
+    const validInput = userSchema.pick({ username: true, password: true })
+    const userExist = req.user
+    if (!validInput.success) {
+        res.status(411).json({ msg: "Incorrect Input" })
+        return
+    }
+    if (!userExist) {
+        res.status(411).json({ msg: "User doesn't exist" })
+        return
+    }
+    const token = jwt.sign(req.body.username, process.env.JWT_SECRET)
+    res.json({ token: token })
 }
--- backend/controllers/validations/user.validation.js
+++ backend/controllers/validations/user.validation.js
@@ -1,6 +1,6 @@
 import z from 'zod'
 
-export const userSignupSchema = z.object({
+export const userSchema = z.object({
     username: z.string().required().min(3).max(30).trim().toLowerCase(),
     password: z.string().required().min(4),
     firstName: z.string().required().max(50).trim(),
