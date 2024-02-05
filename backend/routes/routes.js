5e53622549a775c36693cdf1849e4859221a8cb1
bitbard3 <ansharora3839@gmail.com>
2024-02-06 00:14:54 +0530

    Add account router

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,6 @@
 import express from 'express';
 import userRouter from './user.routes.js'
+import accountRouter from './account.routes.js'
 export const router = express.Router()
 router.use('/user', userRouter)
+router.use('/account', accountRouter)
