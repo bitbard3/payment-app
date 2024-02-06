f40d73636c18e4d13137710fec1cb57cec8e112c
bitbard3 <ansharora3839@gmail.com>
2024-02-07 04:07:12 +0530

    Add transfer route

--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { validUser } from '../middlewares/userAuth.middleware.js'
-import { balance } from '../controllers/account.controllers.js'
+import { balance, transfer } from '../controllers/account.controllers.js'
 
 const router = express.Router()
 
 router.get('/balance', validUser, balance)
+router.post('/transfer', validUser, transfer)
+
 
 export default router   
