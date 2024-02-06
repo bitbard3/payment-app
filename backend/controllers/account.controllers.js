6a1e13f62de5ecb146f377815da05bdcca3be878
bitbard3 <ansharora3839@gmail.com>
2024-02-07 04:06:51 +0530

    Add functionality to transfer funds

--- backend/controllers/account.controllers.js
+++ backend/controllers/account.controllers.js
@@ -1,5 +1,5 @@
 import { Account } from "../db/db.js"
-
+import mongoose from 'mongoose'
 
 
 export const balance = async (req, res) => {
@@ -9,5 +9,33 @@ export const balance = async (req, res) => {
         return res.json({ balance: account.balance })
     } catch (error) {
         return res.status(500).json({ msg: "Internal server error" })
+    }
+}
+export const transfer = async (req, res) => {
+    const userId = req.userId
+    const session = await mongoose.startSession()
+    session.startTransaction()
+    try {
+        const account = await Account.findOne({ userId }).session(session)
+        const balance = account.balance
+        if (!account || balance < req.body.amount) {
+            await session.abortTransaction()
+            return res.status(400).json({ msg: "Insufficent balance" })
+        }
+        try {
+            await Account.findOne({ userId: req.body.to }).session(session)
+        } catch (error) {
+            await session.abortTransaction()
+            return res.status(404).json({ msg: "Account doesnt exist" })
+        }
+        await Account.updateOne({ userId }, { $inc: { balance: - req.body.amount } }).session(session)
+        await Account.updateOne({ userId: req.body.to }, { $inc: { balance: req.body.amount } }).session(session)
+        await session.commitTransaction()
+        return res.json({ msg: 'Transaction successful' })
+    } catch (error) {
+        return res.status(400).json({ msg: "Transaction failed" })
+    }
+    finally {
+        await session.endSession();
     }
 }
