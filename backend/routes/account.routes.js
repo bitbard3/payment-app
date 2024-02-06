25f297beade4d658fc0c2a34cd6ff80b590d5d3b
bitbard3 <ansharora3839@gmail.com>
2024-02-07 02:49:28 +0530

    Add balance route

--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { validUser } from '../middlewares/userAuth.middleware.js'
+import { balance } from '../controllers/account.controllers.js'
 
 const router = express.Router()
 
+router.get('/balance', validUser, balance)
 
 export default router   
