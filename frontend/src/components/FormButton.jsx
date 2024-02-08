c853faf6bf701669f4d83dd83195aaabea43c0cd
bitbard3 <ansharora3839@gmail.com>
2024-02-09 03:42:16 +0530

    Add form button

---
+++ frontend/src/components/FormButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FormButton() {
+  return (
+    <button className="duration-300 font-medium hover:scale-110 hover:bg-primary hover:text-black hover:border-0  px-5 py-1.5 rounded-md border border-primary text-light">
+      Continue
+    </button>
+  );
+}
