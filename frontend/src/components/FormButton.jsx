bd43fd59b16db97e66a6c0838399d67f38b0a3b2
bitbard3 <ansharora3839@gmail.com>
2024-02-09 05:20:55 +0530

    Modify button component

--- frontend/src/components/FormButton.jsx
+++ frontend/src/components/FormButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function FormButton() {
+export default function FormButton({ text }) {
   return (
     <button className="duration-300 font-medium hover:scale-110 hover:bg-primary hover:text-black hover:border-0  px-5 py-1.5 rounded-md border border-primary text-light">
-      Continue
+      {text}
     </button>
   );
 }
