e743391335863abcab186d327af862fc3f0dd138
bitbard3 <ansharora3839@gmail.com>
2024-02-09 05:20:34 +0530

    Modify input component

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function FormInput({ placeholder, type }) {
+export default function FormInput({ placeholder, type, mt }) {
   return (
     <input
       type={type}
       placeholder={placeholder}
-      className="bg-transparent text-light border border-t-0 border-x-0 border-b-light w-3/4 focus:outline-0"
+      className={`bg-transparent text-light border mt-${mt} border-t-0 border-x-0 border-b-light w-3/4 focus:outline-0`}
     />
   );
 }
