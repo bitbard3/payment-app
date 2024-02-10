1fea8df7505763b223f2327c19ba5c34bd765dc9
bitbard3 <ansharora3839@gmail.com>
2024-02-10 16:55:24 +0530

    Add landing route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
+import Landing from "./pages/Landing";
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" Component={Landing}></Route>
         <Route path="/signup" Component={Signup}></Route>
         <Route path="/login" Component={Login}></Route>
       </Routes>
