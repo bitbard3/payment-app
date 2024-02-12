fa70e97f90c9203cfc1755c19f7a10f71539e40e
bitbard3 <ansharora3839@gmail.com>
2024-02-12 18:46:12 +0530

    Make money components responsive

--- frontend/src/components/MoneyHeader.jsx
+++ frontend/src/components/MoneyHeader.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function MoneyHeader() {
   return (
     <div className="flex w-full items-center">
-      <p className="text-light text-lg font-medium">Send money</p>
-      <p className="text-secondary ms-auto text-sm underline-offset-[3px] font-medium underline hover:cursor-pointer z-10">
+      <p className="text-light md:text-lg text-base font-medium">Send money</p>
+      <p className="text-secondary ms-auto md:text-sm text-xs underline-offset-[3px] font-medium underline hover:cursor-pointer z-10">
         See more
       </p>
     </div>
--- frontend/src/components/MoneyIndex.jsx
+++ frontend/src/components/MoneyIndex.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function MoneyIndex() {
   return (
-    <div className="w-full grid grid-cols-10 text-neutral-400 text-sm py-2 bg-dark bg-opacity-80 mt-3 px-3 rounded-md">
-      <div className="col-span-1">
+    <div className="w-full grid grid-cols-10 text-neutral-400 text-xs md:text-sm py-2 bg-dark bg-opacity-80 mt-3 px-3 rounded-md">
+      <div className="col-span-2 md:col-span-1">
         <div className="">#</div>
       </div>
-      <div className="col-span-2">
-        <div className="flex items-center justify-center">
+      <div className="col-span-3 md:col-span-2">
+        <div className="flex items-center md:justify-center">
           <p className="overflow-hidden">First Name</p>
         </div>
       </div>
-      <div className="col-span-2">
-        <div className="flex items-center justify-center">
+      <div className="col-span-3 md:col-span-2">
+        <div className="flex items-center md:justify-center">
           <p className="overflow-hidden">Last Name</p>
         </div>
       </div>
