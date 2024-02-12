eb608aa817e8852e19cdab22aa03fa5251699dff
bitbard3 <ansharora3839@gmail.com>
2024-02-12 14:33:35 +0530

    Add Money index component

---
+++ frontend/src/components/MoneyIndex.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function MoneyIndex() {
+  return (
+    <div className="w-full grid grid-cols-10 text-neutral-400 text-sm py-2 bg-dark bg-opacity-80 mt-3 px-3 rounded-md">
+      <div className="col-span-1">
+        <div className="">#</div>
+      </div>
+      <div className="col-span-2">
+        <div className="flex items-center justify-center">
+          <p className="overflow-hidden">First Name</p>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <div className="flex items-center justify-center">
+          <p className="overflow-hidden">Last Name</p>
+        </div>
+      </div>
+    </div>
+  );
+}
