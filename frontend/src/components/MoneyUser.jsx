5b5c252099acff12a15c2723c3be88cd73d8772d
bitbard3 <ansharora3839@gmail.com>
2024-02-12 14:34:01 +0530

    Add Money user component

---
+++ frontend/src/components/MoneyUser.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function MoneyUser({ firstName, lastName, index }) {
+  return (
+    <>
+      <div className="col-span-1 mt-2">
+        <p>{index}</p>
+      </div>
+      <div className="col-span-2">
+        <div className="flex items-center justify-center mt-2">
+          <p className="overflow-hidden text-ellipsis">{firstName}</p>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <div className="flex items-center justify-center mt-2">
+          <p className="overflow-hidden text-ellipsis">{lastName}</p>
+        </div>
+      </div>
+      <div className="col-span-5">
+        <div className="flex items-center justify-end">
+          <button className="border-secondary  border px-5 rounded-2xl py-1 z-10 slide">
+            <span className="">Pay</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
