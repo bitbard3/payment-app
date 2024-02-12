f87ffb68720dcb0580b3203cd983f12ce6ef0e36
bitbard3 <ansharora3839@gmail.com>
2024-02-12 14:35:05 +0530

    Add dashboard page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
 import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" Component={Landing}></Route>
         <Route path="/signup" Component={Signup}></Route>
         <Route path="/login" Component={Login}></Route>
+        <Route path="/dashboard" Component={Dashboard}></Route>
       </Routes>
     </>
   );
