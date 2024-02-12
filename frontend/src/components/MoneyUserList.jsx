ccdeff9b4cb2a2dbbc885958aab0e89ee9ea163a
bitbard3 <ansharora3839@gmail.com>
2024-02-12 14:34:14 +0530

    Add Money user list component

---
+++ frontend/src/components/MoneyUserList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MoneyUser from "./MoneyUser";
+
+export default function MoneyUserList({ users }) {
+  return (
+    <div className="w-full grid grid-rows-4 grid-cols-10 mt-3 px-3 text-light  gap-5">
+      {users.map((user, index) => {
+        return (
+          <MoneyUser
+            index={index + 1}
+            firstName={user.firstName}
+            lastName={user.lastName}
+          ></MoneyUser>
+        );
+      })}
+    </div>
+  );
+}
