2072b94af5e0f579a29306e5c539b02597a48b3f
bitbard3 <ansharora3839@gmail.com>
2024-02-13 17:56:34 +0530

    Make navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeaderHeading from "./HeaderHeading";
 import HeaderAccount from "./HeaderAccount";
+import NavButton from "./NavButton";
 
 export default function Header({ icon, user, text }) {
   return (
     <div className="flex h-full items-center md:px-16 px-5 w-full">
+      <span className="mr-5">
+        <NavButton></NavButton>
+      </span>
       <HeaderHeading text={text}></HeaderHeading>
       <div className="ms-auto">
         <HeaderAccount icon={icon} user={user}></HeaderAccount>
---
+++ frontend/src/components/NavButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { sideBarOpen } from "../stores/atom/sideBar";
+
+export default function NavButton() {
+  const [sideBar, setSidebar] = useRecoilState(sideBarOpen);
+  return (
+    <button
+      onClick={() => setSidebar((p) => !p)}
+      className="flex flex-col justify-center items-center gap-1"
+    >
+      <span
+        className={`dark:bg-white bg-light block transition-all duration-300 ease-out  h-[2px] w-8 rounded-sm ${
+          !sideBar ? "rotate-45 translate-y-2" : "-translate-y-1"
+        }`}
+      ></span>
+      <span
+        className={`dark:bg-white bg-light block transition-all duration-300 ease-out 
+                    h-[2px] w-8 rounded-sm my-0.5 ${
+                      !sideBar ? "opacity-0" : "opacity-100"
+                    }`}
+      ></span>
+      <span
+        className={`dark:bg-white bg-light block transition-all duration-300 ease-out 
+                    h-[2px] w-8 rounded-sm ${
+                      !sideBar ? "-rotate-45 -translate-y-2" : "translate-y-1"
+                    }`}
+      ></span>
+    </button>
+  );
+}
