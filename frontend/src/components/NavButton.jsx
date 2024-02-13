b6ab831a5f2677487ab223848a20b31dc52b57ce
bitbard3 <ansharora3839@gmail.com>
2024-02-14 00:58:47 +0530

    Change navbutton intial state

--- frontend/src/components/NavButton.jsx
+++ frontend/src/components/NavButton.jsx
@@ -11,19 +11,21 @@ export default function NavButton() {
     >
       <span
         className={`dark:bg-white bg-light block transition-all duration-500 ease-out  h-[2px] w-8 rounded-sm ${
-          !sideBar ? "rotate-45 translate-y-1.5" : "-translate-y-1"
+          !sideBar ? " -translate-y-1" : "rotate-45 translate-y-1.5"
         }`}
       ></span>
       <span
         className={`dark:bg-white bg-light block transition-all duration-500 ease-out 
                     h-[2px] w-8 rounded-sm my-0.5 ${
-                      !sideBar ? "opacity-0" : "opacity-100"
+                      !sideBar ? "opacity-100" : "opacity-0"
                     }`}
       ></span>
       <span
         className={`dark:bg-white bg-light block transition-all duration-500 ease-out 
                     h-[2px] w-8 rounded-sm ${
-                      !sideBar ? "-rotate-45 -translate-y-1.5" : "translate-y-1"
+                      !sideBar
+                        ? " translate-y-1"
+                        : "-rotate-45 -translate-y-1.5"
                     }`}
       ></span>
     </button>
