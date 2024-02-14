cd8764a538fd306393354b98876fec5b2ebf9585
bitbard3 <ansharora3839@gmail.com>
2024-02-14 17:41:50 +0530

    Add edit account header

---
+++ frontend/src/components/EditAccountHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EditAccountHeader() {
+  return (
+    <p className="text-light text-xl md:text-3xl font-medium mt-5">
+      Edit Account
+    </p>
+  );
+}
