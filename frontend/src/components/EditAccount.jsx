3b629731c97a01b3d747c3983cf90a50e7bed6bb
bitbard3 <ansharora3839@gmail.com>
2024-02-14 17:42:16 +0530

    Add edit account component

---
+++ frontend/src/components/EditAccount.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import EditAccountHeader from "./EditAccountHeader";
+import EditAccountInput from "./EditAccountInput";
+import EditAccountButton from "./EditAccountButton";
+
+export default function EditAccount() {
+  return (
+    <div className="bg-dark md:w-[70%] w-[90%] h-[95%] rounded-2xl flex flex-col items-center justify-center">
+      <EditAccountHeader></EditAccountHeader>
+      <div className="w-[80%] pl-10 h-[70%] flex flex-col justify-center gap-10 px-3 md:px-8 mt-16">
+        <EditAccountInput
+          disabled={true}
+          label={"Email"}
+          type={"email"}
+          id={"emailId"}
+        ></EditAccountInput>
+        <EditAccountInput
+          disabled={false}
+          label={"First Name"}
+          type={"text"}
+          id={"firstNameId"}
+        ></EditAccountInput>{" "}
+        <EditAccountInput
+          disabled={false}
+          label={"Last Name"}
+          type={"text"}
+          id={"LastNameId"}
+        ></EditAccountInput>
+        <EditAccountButton></EditAccountButton>
+      </div>
+    </div>
+  );
+}
