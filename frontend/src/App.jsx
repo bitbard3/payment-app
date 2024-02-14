a77e15cf23e9337bb4eb081a2f120f9edb613be3
bitbard3 <ansharora3839@gmail.com>
2024-02-14 17:42:50 +0530

    Add Account page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/signup" Component={Signup}></Route>
         <Route path="/login" Component={Login}></Route>
         <Route path="/dashboard" Component={Dashboard}></Route>
+        <Route path="/account" Component={Account}></Route>
       </Routes>
     </>
   );
---
+++ frontend/src/pages/Account.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import NavItemsList from "../components/NavItemsList";
+import { sideBarOpen } from "../stores/atom/sideBar";
+import { useRecoilValue } from "recoil";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Header from "../components/Header";
+import MobileNav from "../components/MobileNav";
+import Gradients from "../components/Gradients";
+import EditAccount from "../components/EditAccount";
+export default function Account() {
+  const val = false;
+  const sideBar = useRecoilValue(sideBarOpen);
+  return (
+    <div>
+      <div className="bg-dark h-screen w-screen grid grid-cols-12 grid-rows-9 relative overflow-hidden">
+        <div
+          className={`col-span-2 bg-box bg-opacity-40 row-span-9 hidden ${
+            !sideBar ? `hidden` : `xl:block `
+          }`}
+        >
+          <div className="flex flex-col gap-10 h-full">
+            <NavItemsList></NavItemsList>
+          </div>
+        </div>
+        <div
+          className={`row-span-1 col-span-12  bg-box bg-opacity-30 ${
+            !sideBar ? ` col-span-12` : `xl:col-span-10`
+          }`}
+        >
+          <Header text={"Account"} icon={faHouse} buttonText={"Home"}></Header>
+        </div>
+        <div
+          className={`row-span-8 mt-10 bg-box bg-opacity-35 md:mx-10 xl:mx-0 md:rounded-2xl mb-5 flex flex-col items-center justify-center ${
+            !sideBar
+              ? `xl:col-start-2 col-span-12 xl:col-span-10`
+              : `xl:col-start-4 xl:col-span-8 col-span-12`
+          }`}
+        >
+          <EditAccount></EditAccount>
+        </div>
+        <MobileNav></MobileNav>
+        <Gradients></Gradients>
+      </div>
+    </div>
+  );
+}
