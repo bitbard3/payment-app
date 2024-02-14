64962830d1519a32f8b9bf6f0277fff99d43a013
bitbard3 <ansharora3839@gmail.com>
2024-02-14 17:41:27 +0530

    Add edit account button

---
+++ frontend/src/components/EditAccountButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EditAccountButton() {
+  return (
+    <button className="text-white border border-primary px-10 text-sm md:px-12 w-1/12 flex justify-center ms-auto md:mt-8 mt-4 rounded-2xl md:rounded-3xl py-1.5 z-10 slide-primary">
+      Update
+    </button>
+  );
+}
