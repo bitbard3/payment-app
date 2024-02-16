77f010cbe0f01ecbfa34fae276d04111d1b0db2b
bitbard3 <ansharora3839@gmail.com>
2024-02-16 12:00:58 +0530

    Add transaction header component

---
+++ frontend/src/components/TransactionsHeaders.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TransactionsHeaders() {
+  return (
+    <div className="flex w-full items-center">
+      <p className="text-light md:text-lg text-base font-medium">
+        Transactions History
+      </p>
+    </div>
+  );
+}
