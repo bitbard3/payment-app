cde4c0683146c2d653bddefd57aa07a5f49aee51
bitbard3 <ansharora3839@gmail.com>
2024-02-19 15:46:19 +0530

    Add loader to form button

--- frontend/src/components/FormButton.jsx
+++ frontend/src/components/FormButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function FormButton({ text, onClick }) {
+export default function FormButton({ text, onClick, disabled }) {
   return (
     <button
       onClick={onClick}
-      className=" font-medium slide-form px-5 py-1.5 rounded-md border border-purple text-light"
+      disabled={disabled}
+      className=" font-medium flex items-center gap-3 justify-center enabled:slide-form disabled:border-opacity-70 disabled:text-neutral-400 px-5 py-1.5 rounded-md border border-purple text-light"
     >
+      {disabled && (
+        <div
+          className="animate-spin inline-block size-5 border-[3px] border-current border-t-transparent rounded-full my-1"
+          role="status"
+          aria-label="loading"
+        ></div>
+      )}
       {text}
     </button>
   );
