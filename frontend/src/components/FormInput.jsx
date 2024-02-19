cf2123c4d5d5a94b64355bd852ce2ff826b4280b
bitbard3 <ansharora3839@gmail.com>
2024-02-19 15:22:50 +0530

    Add props in form input

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function FormInput({ placeholder, type, mt }) {
+export default function FormInput({ placeholder, type, mt, onChange, value }) {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      required
       className={`bg-transparent text-light border mt border-t-0 ${mt} border-x-0 border-b-light w-3/4 focus:outline-0`}
     />
   );
