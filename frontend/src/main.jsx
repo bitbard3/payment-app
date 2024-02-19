43d6c273fde3aa7100b660f591d38dcdeb5cbd9b
bitbard3 <ansharora3839@gmail.com>
2024-02-19 15:45:24 +0530

    Add toaster to root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { Toaster } from "@/components/ui/toaster";
 import { RecoilRoot } from "recoil";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
         <App />
+        <Toaster />
       </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
