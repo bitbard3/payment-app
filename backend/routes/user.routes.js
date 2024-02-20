c7d9c5267cc714ecc16abfed9f13f6e8ed09ea7a
bitbard3 <ansharora3839@gmail.com>
2024-02-20 23:34:34 +0530

    Add /addFriendRequest route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { userExist } from '../middlewares/userExist.middleware.js'
-import { signup, login, update, bulk, userInfo } from '../controllers/user.controllers.js'
+import { signup, login, update, bulk, userInfo, addFriendRequest } from '../controllers/user.controllers.js'
 import { validUser } from '../middlewares/userAuth.middleware.js'
 import { userLogin } from '../middlewares/userLogin.middleware.js'
 
@@ -12,4 +12,5 @@ router.post('/login', userLogin, login)
 router.put('/', validUser, update)
 router.get('/bulk', validUser, bulk)
 router.get('/userInfo/:userId', userInfo)
+router.post('/addFriendRequest', validUser, addFriendRequest)
 export default router
