a9b5ce24dccdb6db789ea17af8aef24844feee1a
bitbard3 <ansharora3839@gmail.com>
2024-02-20 17:49:55 +0530

    Change validation schema

--- backend/controllers/validations/account.validation.js
+++ backend/controllers/validations/account.validation.js
@@ -1,6 +1,7 @@
 import z from 'zod'
 
 export const transferSchmea = z.object({
-    to: z.string().length(24),
+    sender: z.string().length(24),
+    receiver: z.string().length(24),
     amount: z.number().positive()
 })
