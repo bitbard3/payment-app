b6d4a303dcab86c7cd727655792b2bdbcea1668c
bitbard3 <ansharora3839@gmail.com>
2024-02-21 02:42:36 +0530

    Add /transactions route

--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { validUser } from '../middlewares/userAuth.middleware.js'
-import { transfer } from '../controllers/transaction.controllers.js'
+import { transactions, transfer } from '../controllers/transaction.controllers.js'
 
 const router = express.Router()
 router.post('/transfer', validUser, transfer)
+router.get('/transactions', validUser, transactions)
 
 
 export default router 
