2fd1b4c843b03399f123131cfb6bd34c74ccdabd
bitbard3 <ansharora3839@gmail.com>
2024-02-20 18:01:20 +0530

    Make transfer route available only on login

--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -3,7 +3,7 @@ import { validUser } from '../middlewares/userAuth.middleware.js'
 import { transfer } from '../controllers/transaction.controllers.js'
 
 const router = express.Router()
-router.post('/transfer', transfer)
+router.post('/transfer', validUser, transfer)
 
 
 export default router 
