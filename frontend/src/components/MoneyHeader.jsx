2deae1fb7f3347a5b3cd1b826669625c74c64354
bitbard3 <ansharora3839@gmail.com>
2024-02-23 15:01:51 +0530

    Make add friend ui

--- frontend/src/components/MoneyHeader.jsx
+++ frontend/src/components/MoneyHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function MoneyHeader({ hidden }) {
+export default function MoneyHeader({}) {
   return (
     <div className="flex w-full items-center">
       <p className="text-light md:text-lg text-base font-medium">Send money</p>
       <p
-        className={`text-purple ms-auto md:text-sm text-xs underline-offset-[3px] font-medium underline hover:cursor-pointer z-10 ${hidden}`}
+        className={`text-purple ms-auto md:text-sm text-xs underline-offset-[3px] font-medium underline hover:cursor-pointer z-10`}
       >
         See more
       </p>
