a57372c5b36274e9391ef35580a69f3945922328
bitbard3 <ansharora3839@gmail.com>
2024-02-23 15:15:25 +0530

    Separate Money list and friend list as tabs

--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MoneyIndex from "./MoneyIndex";
 import MoneyUserList from "./MoneyUserList";
 import PaginationPrevButton from "./PaginationPrevButton";
@@ -6,12 +6,19 @@ import PaginationNextButton from "./PaginationNextButton";
 import SendMoneyHeader from "./SendMoneyHeader";
 import AddFriendList from "./AddFriendList";
 export default function SendMoney({ users }) {
+  const [currentTab, setCurrentTab] = useState("money");
   return (
     <div className="flex flex-col w-full h-full py-5 px-6">
-      <SendMoneyHeader></SendMoneyHeader>
+      <SendMoneyHeader
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+      ></SendMoneyHeader>
       <MoneyIndex></MoneyIndex>
-      <AddFriendList friends={users}></AddFriendList>
-      {/* <MoneyUserList users={users}></MoneyUserList> */}
+      {currentTab == "money" ? (
+        <MoneyUserList users={users}></MoneyUserList>
+      ) : (
+        <AddFriendList friends={users}></AddFriendList>
+      )}
       <div className="ml-auto mr-auto mt-auto ">
         <PaginationPrevButton></PaginationPrevButton>
         <PaginationNextButton></PaginationNextButton>
--- frontend/src/components/SendMoneyHeader.jsx
+++ frontend/src/components/SendMoneyHeader.jsx
@@ -1,46 +1,54 @@
 import React from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
-export default function SendMoneyHeader({ active }) {
+export default function SendMoneyHeader({ currentTab, setCurrentTab }) {
   return (
     <div className="flex flex-col items-end gap-2 md:gap-0">
       <div className="flex w-full items-center">
         <button
+          onClick={() => {
+            setCurrentTab("money");
+          }}
           className={`z-10 border px-3 py-1.5
           ${
-            active
+            currentTab == "money"
               ? ` border-b-purple  border-x-0 border-t-0`
-              : `border-0 rounded-md hover:bg-neutral-300 hover:bg-opacity-15 duration-500 hover:scale-[0.98] `
+              : `border-0 rounded-md hover:bg-neutral-300 hover:bg-opacity-15 hover:scale-[0.98] `
           }`}
         >
           <p className="text-light md:text-lg text-base font-medium">Send</p>
         </button>
         <button
+          onClick={() => {
+            setCurrentTab("friends");
+          }}
           className={`z-10 border px-3 py-1.5 md:ml-10 ml-3
           ${
-            active
-              ? ` border-b-purple  border-x-0 border-t-0`
-              : `border-0  rounded-md hover:bg-neutral-300 hover:bg-opacity-15 duration-500 hover:scale-[0.98] `
+            currentTab == "friends"
+              ? ` border-b-purple  border-x-0 border-t-0 duration-0`
+              : `border-0  rounded-md hover:bg-neutral-300 hover:bg-opacity-15 hover:scale-[0.98] `
           }`}
         >
           <p className="text-light md:text-lg text-base font-medium">Add</p>
         </button>
       </div>
-      <div className="md:w-1/2 w-full mb-1 md:mb-0">
-        <div class="relative w-full">
-          <input
-            id="default-search"
-            class="block w-full p-2 text-sm ps-5 text-gray-900  rounded-lg bg-gray-50"
-            placeholder='Try  "test" . . . '
-            required
-          />
-          <button
-            type="submit"
-            class="text-white absolute end-1 bottom-0.5 focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm px-2 py-1 "
-          >
-            <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
-          </button>
+      {currentTab == "friends" && (
+        <div className="md:w-1/2 w-full mb-1 md:mb-0">
+          <div class="relative w-full">
+            <input
+              id="default-search"
+              class="block w-full p-2 text-sm ps-5 text-gray-900  rounded-lg bg-gray-50"
+              placeholder='Try  "test" . . . '
+              required
+            />
+            <button
+              type="submit"
+              class="text-white absolute end-1 bottom-0.5 focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm px-2 py-1 "
+            >
+              <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
