c604d23c5cd075078a1a73ceb8d34d7e9049590e
bitbard3 <ansharora3839@gmail.com>
2024-02-24 16:04:06 +0530

    Handle empty state

--- frontend/src/components/AddFriendList.jsx
+++ frontend/src/components/AddFriendList.jsx
@@ -4,17 +4,24 @@ import AddFriend from "./AddFriend";
 export default function AddFriendList({ friends }) {
   return (
     <>
-      <div className="w-full grid grid-cols-10 mt-3 px-3 text-light  gap-5 text-sm md:text-base">
-        {friends.map((friend, index) => {
-          return (
-            <AddFriend
-              index={index + 1}
-              firstName={friend.firstName}
-              lastName={friend.lastName}
-            ></AddFriend>
-          );
-        })}
-      </div>
+      {friends.length > 0 ? (
+        <div className="w-full grid grid-cols-10 mt-3 px-3 text-light gap-5 text-sm md:text-base">
+          {friends.map((friend, index) => {
+            return (
+              <AddFriend
+                key={index}
+                index={index + 1}
+                firstName={friend.firstName}
+                lastName={friend.lastName}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex w-full h-full justify-center items-center">
+          <p className="text-2xl text-light">Search user</p>
+        </div>
+      )}
     </>
   );
 }
