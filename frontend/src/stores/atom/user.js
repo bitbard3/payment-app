016f49ed1e1422d7d9459aaf816001cfb245b59f
bitbard3 <ansharora3839@gmail.com>
2024-02-25 18:17:35 +0530

    Change user atom according to new schema

--- frontend/src/stores/atom/user.js
+++ frontend/src/stores/atom/user.js
@@ -20,6 +20,7 @@ export const fetchUser = selector({
                 lastName: user.data.userData.lastName,
                 balance: user.data.userData.balance,
                 friends: [...user.data.userData.friends],
+                sentFriendRequests: [...user.data.userData.sentFriendRequests],
                 friendRequests: [...user.data.userData.friendRequests],
                 transactions: [...transactions.data.transactions],
                 friendsLength: [...user.data.userData.friends].length,
