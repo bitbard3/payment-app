86fb9f6d23a8ed14f64013ef3a430cc738361caf
bitbard3 <ansharora3839@gmail.com>
2024-02-26 02:23:27 +0530

    Add ui of friend request list

---
+++ frontend/src/components/AddFriendRequest.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { UserPlusIcon, UserMinusIcon } from "@heroicons/react/16/solid";
+export default function AddFriendRequest({
+  firstName,
+  lastName,
+  onAddHandler,
+}) {
+  return (
+    <div className="flex  justify-between border-b-[1px] py-5">
+      <p>{firstName}</p>
+      <p>{lastName}</p>
+      <div className="flex gap-4">
+        <button onClick={onAddHandler} className="">
+          <UserPlusIcon className="h-6 w-6 text-gray-500 opacity-80" />
+        </button>
+        <button className="">
+          <UserMinusIcon className="h-6 w-6 text-gray-500 opacity-80" />
+        </button>
+      </div>
+    </div>
+  );
+}
