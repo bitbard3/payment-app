678fad0b193dfbc8e95f08e78c4a7281f86c77ee
bitbard3 <ansharora3839@gmail.com>
2024-02-28 02:21:38 +0530

    Add add money schmea

--- backend/controllers/validations/account.validation.js
+++ backend/controllers/validations/account.validation.js
@@ -4,3 +4,6 @@ export const transferSchmea = z.object({
     receiver: z.string().length(24),
     amount: z.number().positive()
 })
+export const addMoneySchema = z.object({
+    amount: z.number().int().lte(9999)
+})
