aa38846f8c0af33969b073c6d2048a97ad967bfa
bitbard3 <ansharora3839@gmail.com>
2024-03-01 06:10:04 +0530

    Add Skeletons in money component

--- frontend/src/components/Money.jsx
+++ frontend/src/components/Money.jsx
@@ -3,17 +3,19 @@ import MoneyHeader from "./MoneyHeader";
 import MoneyIndex from "./MoneyIndex";
 import MoneyUserList from "./MoneyUserList";
 import friend from "../images/friend.svg";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { user } from "@/stores/atom/user";
 import { useNavigate } from "react-router-dom";
-
+import { Skeleton } from "./ui/skeleton";
 export default function Money() {
   const navigate = useNavigate();
-  const userInfo = useRecoilValue(user);
+  const userInfo = useRecoilValueLoadable(user);
   return (
     <div className="flex flex-col w-full h-full py-5 px-6 relative overflow-scroll">
       <MoneyHeader></MoneyHeader>
-      {userInfo.friendsLength == 0 ? (
+      {userInfo.state == "loading" ? (
+        <Skeleton className="bg-gray-700 mt-auto h-[80%] w-[100%]"></Skeleton>
+      ) : userInfo.contents.friendsLength == 0 ? (
         <>
           <img src={friend} className="h-1/2 mt-auto mb-auto" alt="" />
           <div className="mr-auto ml-auto mb-auto">
