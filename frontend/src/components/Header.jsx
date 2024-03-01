5b60537258ced89a9a2ab8342ae48e10af469503
bitbard3 <ansharora3839@gmail.com>
2024-03-01 05:51:16 +0530

    Add skeletons loader in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useRecoilValueLoadable } from "recoil";
 import { user } from "@/stores/atom/user";
 import { QrCodeIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   Popover,
   PopoverContent,
@@ -21,29 +22,38 @@ export default function Header({ icon, text }) {
         <NavButton></NavButton>
       </span>
       <HeaderHeading text={text}></HeaderHeading>
-      <button
-        onClick={() => navigate("/account")}
-        className="md:absolute md:right-[28%] xl:right-[17%] lg:right-[20%] ml-auto bg-box px-2 py-2 rounded-full z-10"
-      >
-        <QrCodeIcon className="h-6 w-6 text-white" />
-      </button>
-      <div className="ms-auto relative">
+
+      <div className="ms-auto flex items-center justify-center relative">
+        <button
+          onClick={() => navigate("/account")}
+          className="bg-box px-2 py-2 rounded-full z-10 mr-3"
+        >
+          <QrCodeIcon className="h-6 w-6 text-white" />
+        </button>
         <Popover>
           <PopoverTrigger>
             <HeaderAccount
               icon={icon}
-              user={userAtomLoadable.contents.firstName}
+              user={
+                userAtomLoadable.state === "loading"
+                  ? null
+                  : userAtomLoadable.contents.firstName
+              }
             ></HeaderAccount>
           </PopoverTrigger>
           <PopoverContent>
             <HeaderPopover
-              friendsReq={userAtomLoadable.contents.friendRequestsLength}
-            ></HeaderPopover>
+              friendsReq={
+                userAtomLoadable.state === "hasValue" &&
+                userAtomLoadable.contents.friendRequestsLength
+              }
+            />
           </PopoverContent>
         </Popover>
-        {userAtomLoadable.contents.friendRequestsLength > 0 && (
-          <div className="bg-[#b73a3a] absolute h-3 w-3 rounded-full top-[2px] -left-[3px]"></div>
-        )}
+        {userAtomLoadable.state === "hasValue" &&
+          userAtomLoadable.contents.friendRequestsLength > 0 && (
+            <div className="bg-[#b73a3a] absolute h-3 w-3 rounded-full top-[2px] -left-[3px]"></div>
+          )}
       </div>
     </div>
   );
--- frontend/src/components/HeaderAccount.jsx
+++ frontend/src/components/HeaderAccount.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 export default function HeaderAccount({ user, icon }) {
   return (
     <div className="rounded-lg flex items-center gap-3 bg-box bg-opacity-60 px-6 py-2">
       {icon}
       <span className="text-light font-medium text-sm md:text-base">
-        {user}
+        {user ? (
+          user
+        ) : (
+          <Skeleton className=" bg-gray-400 w-16 h-5 rounded-xl"></Skeleton>
+        )}
       </span>
     </div>
   );
