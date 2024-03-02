4f308492e2da7a5885f78cb3e93d45cfe2b33861
bitbard3 <ansharora3839@gmail.com>
2024-03-03 04:14:05 +0530

    Create private route util

---
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import axios from "axios";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const PrivateRoutes = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    async function checkToken() {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) throw new Error("Token not found");
+        const res = await axios.get(
+          "https://payment-app-topaz.vercel.app/api/v1/user/validUser",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        setIsLoggedIn(true);
+      } catch (error) {
+        setIsLoggedIn(false);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    checkToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="h-screen w-screen bg-dark flex items-center justify-center">
+          <Skeleton className="h-[95%] w-[95%] bg-gray-600"></Skeleton>
+        </div>
+      </>
+    );
+  }
+
+  return isLoggedIn ? (
+    <Outlet />
+  ) : (
+    <>
+      <Navigate to="/login" />
+    </>
+  );
+};
+
+export default PrivateRoutes;
