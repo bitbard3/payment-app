38858fee8ffc248ebe33fd7d7aa29bd0b84a1dad
bitbard3 <ansharora3839@gmail.com>
2024-03-03 06:42:41 +0530

    Deploy

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express()
 const port = 3000
 
-app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_URL }))
 app.use(express.json())
 app.use('/api/v1', router)
 
